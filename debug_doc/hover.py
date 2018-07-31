e79d899aec3516aa6b7e5575cf3309aa90c1bd24
Lee <lee_1024@yeah.net>
2018-07-31 09:13:25 +0800

    调试新的悬停

--- debug_doc/hover.py
+++ debug_doc/hover.py
@@ -5,17 +5,23 @@ from selenium.webdriver.common.action_chains import ActionChains
 import time
 driver = webdriver.Chrome()
 
-driver.get('https://community.stg.hnacloudmarket.com/')
+driver.get('https://stg.hnacloudmarket.com/')
 driver.maximize_window()
-time.sleep(5)
-elem = driver.find_element_by_xpath('//div[@class="titleMain"]/ul/li[1]/span')
-ActionChains(driver).move_to_element(elem).perform()
-time.sleep(5)
+time.sleep(3)
+driver.find_element_by_class_name('apsClose').click()
+elem1 = driver.find_element_by_xpath('//div[@class="titleMain"]/ul/li[1]/span')
+ActionChains(driver).move_to_element(elem1).perform()
+time.sleep(1)
+elem2 = driver.find_element_by_xpath('//div[@class="titleSpan"]/div[2]/ul/li[1]/ul/li[4]')
+ActionChains(driver).move_to_element(elem2).perform()
+time.sleep(1)
+elem3 = driver.find_element_by_xpath('//div[@class="titleSpan"]/div[2]/ul/li[2]/ul/li[1]')
+ActionChains(driver).move_to_element(elem3).perform()
+elem4 = driver.find_element_by_xpath('//div[@class="titleSpan"]/div[2]/ul/li[3]/div/span[1]')
+ActionChains(driver).move_to_element(elem4).perform()
+driver.find_element_by_xpath('//div[@class="titleSpan"]/div[2]/ul/li[3]/div/span[1]').click()
+time.sleep(8)
 
-checks = ['ECS',u'华为',u'金山',u'华云',u'创业',u'CVM']
-
-for i in checks:
-    print i
 
 
 
