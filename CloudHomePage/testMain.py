e95ec9afed4248473849d19f9dcd372490b3350f
Lee <lee_1024@yeah.net>
2018-08-28 10:04:56 +0800

    挨个调试

--- CloudHomePage/testMain.py
+++ CloudHomePage/testMain.py
@@ -16,8 +16,8 @@ testunit = unittest.TestSuite()
 #testunit.addTest(unittest.makeSuite(Shopping.Shopping))
 #testunit.addTest(unittest.makeSuite(OnlinePlay.OlinePlay))
 #testunit.addTest(unittest.makeSuite(Product.Product))
-testunit.addTest(unittest.makeSuite(Company.Company))
-#testunit.addTest(unittest.makeSuite(ToBuy.ToBuy))
+#testunit.addTest(unittest.makeSuite(Company.Company))
+testunit.addTest(unittest.makeSuite(ToBuy.ToBuy))
 #testunit.addTest(unittest.makeSuite(OtherTest.OtherTest))
 #定义一个报告存放路径
 filepath = "C:\\AutoTestNew\\AutoTest\\CloudHomePage\\Report\\result"+now+".html"
