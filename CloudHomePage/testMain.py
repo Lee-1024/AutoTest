76c9a8ee3b36f6cc5f4445c0fe6c3c57208c7de4
Lee <lee_1024@yeah.net>
2018-08-28 11:25:22 +0800

    添加修改信息记录

--- CloudHomePage/testMain.py
+++ CloudHomePage/testMain.py
@@ -12,13 +12,13 @@ now=time.strftime("%Y%m%d%H%S%S",time.localtime())
 #定义测试容器
 testunit = unittest.TestSuite()
 #将测试用例添加到容器中
-#testunit.addTest(unittest.makeSuite(TitleSpan.TitleSpan))
+testunit.addTest(unittest.makeSuite(TitleSpan.TitleSpan))
 #testunit.addTest(unittest.makeSuite(Shopping.Shopping))
-#testunit.addTest(unittest.makeSuite(OnlinePlay.OlinePlay))
-#testunit.addTest(unittest.makeSuite(Product.Product))
-#testunit.addTest(unittest.makeSuite(Company.Company))
-testunit.addTest(unittest.makeSuite(ToBuy.ToBuy))
-#testunit.addTest(unittest.makeSuite(OtherTest.OtherTest))
+testunit.addTest(unittest.makeSuite(OnlinePlay.OlinePlay))
+testunit.addTest(unittest.makeSuite(Product.Product))
+testunit.addTest(unittest.makeSuite(Company.Company))
+#testunit.addTest(unittest.makeSuite(ToBuy.ToBuy))
+testunit.addTest(unittest.makeSuite(OtherTest.OtherTest))
 #定义一个报告存放路径
 filepath = "C:\\AutoTestNew\\AutoTest\\CloudHomePage\\Report\\result"+now+".html"
 fp = file(filepath,"wb")
